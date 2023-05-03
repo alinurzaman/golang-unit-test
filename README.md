19bb1a1164560d1fe793fdf7a6dc56bd4f2a3e64
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-05-03 08:15:50 +0700

    handle failed test

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # HOW TO RUN
 
-- ``go test`` (run all unit test)
-- ``go test -v`` (run all unit test with details)
-- ``go test -v -run=TestFunctionName`` (run specific unit test)
-- ``go test ./...`` (run from parent folder)
+- `go test` (run all unit test)
+- `go test -v` (run all unit test with details)
+- `go test -v -run=TestFunctionName` (run specific unit test)
+- `go test ./...` (run from parent folder)
+
+# HOW TO HANDLE FAILED TEST
+
+- `t.Fail()` (fail the unit test but continue to run the rest code)
+- `t.FailNow()` (fail the unit test and stop run the test)
+- `t.Error()` (show the error log then call t.Fail())
+- `t.Fatal()` (show the error log then call t.FailNow())
--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,11 +1,51 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
-	result := HelloWorld("Ali")
-	if result != "Hello Ali" {
+	result := HelloWorld("World")
+	if result != "Hello World" {
 		// unit test failed
-		panic("Result is not Hello Ali")
+		t.Fail()
 	}
+	fmt.Println("TestHelloWorld done.")
+}
+
+func TestHiWorld(t *testing.T) {
+	result := HelloWorld("World")
+	if result != "Hi World" {
+		// unit test failed
+		t.Fail()
+	}
+	fmt.Println("TestHiWorld done.")
+}
+
+func TestHiAli(t *testing.T) {
+	result := HelloWorld("Ali")
+	if result != "Hi Ali" {
+		// unit test failed
+		t.FailNow()
+	}
+	fmt.Println("TestHiAli done.")
+}
+
+func TestHiWorldError(t *testing.T) {
+	result := HelloWorld("World")
+	if result != "Hi World" {
+		// unit test failed
+		t.Error("Result should be Hi World")
+	}
+	fmt.Println("TestHiWorld done.")
+}
+
+func TestHiWorldFatal(t *testing.T) {
+	result := HelloWorld("World")
+	if result != "Hi World" {
+		// unit test failed
+		t.Fatal("Result should be Hi World")
+	}
+	fmt.Println("TestHiWorld done.")
 }
